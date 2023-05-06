5a8a2fdf8b3343cd7cf34bb6d77210afd5768a0c
Alexander Hornstrup Pødenphant <aluxpoed@gmail.com>
2023-05-06 12:01:43 +0200

    Managing Recipes in a Recipe Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
     selector: 'app-recipe-list',
@@ -7,13 +8,15 @@ import { Recipe } from "../recipe.model";
     styleUrls: ['./recipe-list.component.css']
 })
 
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
     @Output() recipeWasSelected = new EventEmitter<Recipe>();
+    recipes: Recipe[];
 
-    recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg' ),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg' )
-    ];
+    constructor(private recipeService: RecipeService){}
+
+    ngOnInit(): void {
+      this.recipes = this.recipeService.getRecipes();
+    }
 
     onRecipeSelected(recipe: Recipe){
         this.recipeWasSelected.emit(recipe);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,12 @@
-export class RecipeService {
+import { Recipe } from "./recipe.model";
 
-  constructor() { }
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg' ),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg' )
+];
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
 }
