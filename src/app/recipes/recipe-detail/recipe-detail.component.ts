b599a3e5f287cfefdb77fcde615d0f1632529ca8
Alexander Hornstrup Pødenphant <aluxpoed@gmail.com>
2023-05-11 13:26:30 +0200

    Configuring Route Parameters

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
-import { Ingredient } from "src/app/shared/ingredient.model";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
     selector: 'app-recipe-detail',
@@ -9,10 +9,22 @@ import { Ingredient } from "src/app/shared/ingredient.model";
     styleUrls: ['./recipe-detail.component.html']
 })
 
-export class RecipeDetailComponent {
-  @Input() recipe: Recipe
+export class RecipeDetailComponent implements OnInit{
+  recipe: Recipe
+  id: number;
 
-  constructor(private recipeService: RecipeService){}
+  constructor(private recipeService: RecipeService,
+    private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      )
+  }
 
   onAddToShoppingList(){
     this.recipeService.addIngredientToShoppingList(this.recipe.ingredients);
